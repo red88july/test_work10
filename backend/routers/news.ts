ffd3734f61a3a421ec03636ac870f6d381713351
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-02-03 14:16:12 +0600

    "Create POST,GET,GET by id and DELETE by id for 'news'"

--- backend/routers/news.ts
+++ backend/routers/news.ts
@@ -1,8 +1,8 @@
 import {Router} from "express";
-import {NewsPost} from "../types";
-import {imagesUpload} from "../multer";
-import serverMySQL from "../serverMySQL";
 import {RowDataPacket} from "mysql2";
+import serverMySQL from "../serverMySQL";
+import {imagesUpload} from "../multer";
+import {NewsPost} from "../types";
 
 export const newsRouter = Router();
 
@@ -19,8 +19,9 @@ newsRouter.post('/', imagesUpload.single('image'), async (req, res, next) => {
             image: req.file ? req.file.filename : null
         }
 
-        const [results, error] = await serverMySQL.getConnection().query(
-            'INSERT INTO news SET ?', News) as RowDataPacket[];
+        const [results, error] =
+            await serverMySQL.getConnection().query(
+                'INSERT INTO news SET ?', News) as RowDataPacket[];
 
         if (error) {
             console.error('Error inserting news:', error);
@@ -28,6 +29,64 @@ newsRouter.post('/', imagesUpload.single('image'), async (req, res, next) => {
         } else {
             const insertNews = {...News, id: results.insertId};
             res.status(201).json(insertNews);
+        }
+
+    } catch (e) {
+        return next(e);
+    }
+});
+
+newsRouter.get('/', async (req, res) => {
+
+    try {
+        const [results] =
+            await serverMySQL.getConnection().query(
+                'SELECT news.id, news.image, news.title, news.datetime FROM news;');
+
+        res.status(200).json(results);
+
+    } catch (error) {
+        console.error('Error GET items:', error);
+        res.status(500).send({error: 'Internal Server Error'});
+    }
+});
+
+newsRouter.get('/:id', async (req, res, next) => {
+
+    try {
+
+        const [results, error] =
+            await serverMySQL.getConnection().query(
+                'SELECT news.id, news.title, news.datetime, news.content, news.image FROM news ' +
+                'WHERE news.id = ?; ',
+                [req.params.id],) as RowDataPacket[];
+
+        const news = results[0];
+
+        if (!news) {
+            return res.status(404).send({error: 'News not found!'});
+        }
+        res.status(200).json(results);
+
+        if (error) {
+            console.error('Error get by id news:', error);
+            res.status(500).send({error: 'Internal server error'})
+        }
+
+    } catch (e) {
+        return next(e);
+    }
+
+});
+
+newsRouter.delete('/:id', async (req, res, next) => {
+    try {
+        const [result] = await serverMySQL.getConnection().query(
+            'DELETE FROM news WHERE id = ? LIMIT 1;',
+            [req.params.id]) as RowDataPacket[];
+
+        if (result) {
+            res.status(200).send({success: "Resource on items table was been deleted!"});
         }
 
     } catch (e) {
